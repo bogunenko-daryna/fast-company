4a2a3abdc687d6e8d6225a496e8c9a220cce796a
unknown <bogunenkodarina@gmail.com>
2021-12-07 19:18:06 +0200

    filter professions

---
+++ src/app/components/groupList.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import PropTypes from "prop-types";
+const GroupList = ({
+    items,
+    valuePtoperty,
+    contentProperty,
+    onItemSelect,
+    selectedItem
+}) => {
+    return (
+        <ul className="list-group">
+            {Object.keys(items).map((item) => (
+                <li
+                    key={items[item][valuePtoperty]}
+                    onClick={() => onItemSelect(items[item])}
+                    className={
+                        "list-group-item" +
+                        (items[item] === selectedItem ? " active" : "")
+                    }
+                    role="button"
+                >
+                    {items[item][contentProperty]}
+                </li>
+            ))}
+        </ul>
+    );
+};
+
+GroupList.defaultProps = {
+    valuePtoperty: "_id",
+    contentProperty: "name"
+};
+
+GroupList.propTypes = {
+    items: PropTypes.object.isRequired,
+    valuePtoperty: PropTypes.string.isRequired,
+    contentProperty: PropTypes.string.isRequired,
+    selectedItem: PropTypes.object,
+    onItemSelect: PropTypes.func
+};
+
+export default GroupList;
